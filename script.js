7045aa5dde9aa2572640c289f3e08afa79cae0e5
DrikaDev <adriguti.adriana@hotmail.com>
2022-05-09 19:32:24 -0300

    alteracao

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
 function validaFormulario(){
-  if(document.getElementById("nome").value != "" &&                  document.getElementById("email").value != "" &&                 document.getElementById("telefone").value != ""){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" &&                 document.getElementById("telefone").value != ""){
     alert("Prontinho! Você receberá as novidades por email.")  
   }else{
     alert("Por favor, preencha os campos nome, email e telefone!")
