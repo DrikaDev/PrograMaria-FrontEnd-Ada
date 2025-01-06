4e479e30f89b8dc24fd739ed752ad26df7248aaa
drikadev <adriguti.adriana@hotmail.com>
2025-01-06 17:23:25 -0300

    Correção do html e melhorias no JS

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-document.getElementById("botaoEnviar").addEventListener("click",validaFormulario)
+const botao = document.getElementById("botaoEnviar");
 
-function validaFormulario(){
-  if(document.getElementById("nome").value != "" && 
-    document.getElementById("email").value != "" && 
-    document.getElementById("telefone").value != ""){
-    alert("Prontinho! Você receberá as novidades por email.");
-  }else{
-    alert("Por favor, preencha os campos nome, email e telefone!");
-  }
+if (botao) {
+  botao.addEventListener("click", validarFormulario);
 }
+
+function validarFormulario(event){
+  event.preventDefault(); //impede o comportamento padrão do botão de enviar
+  const nome = document.getElementById("nome").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const telefone = document.getElementById("telefone").value.trim();
+
+  if(nome === "" ){
+    alert("Por favor, preencha o campo nome!");
+    return;
+  }
+  if(email === ""){
+    alert("Por favor, preencha o campo email!");
+    return;
+  }
+  if(telefone === ""){
+    alert("Por favor, preencha o campo telefone!");
+    return;
+  }
+  alert("Pronto! Você receberá as novidades por email.");
+  
+  // Limpa os campos do formulário após o envio dos dados:
+  document.getElementById("nome").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("telefone").value = "";
+}
+
